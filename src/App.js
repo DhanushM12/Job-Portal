a640f1216bf966d48451cccdbe301f21531b6297
DhanushM12 <dhanushmukhi@gmail.com>
2020-10-16 18:54:02 +0530

    useFetchJobs added and handled error & loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
   return (
-    <div className="App">
-      <h1>Welcome to the Job Portal</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading Jobs...</h1>}
+      {error && <h1>Error, Please Try Refreshing...</h1>}
+      {<h1>{jobs.length}</h1>}
+    </Container>
   );
 }
 
