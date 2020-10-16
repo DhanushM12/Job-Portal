39b2345f1f8bafb53d7490b16ac243e202047d18
DhanushM12 <dhanushmukhi@gmail.com>
2020-10-16 21:23:16 +0530

    added the job pagination UI and functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
 import Job from "./Job";
+import Page from "./Page";
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  const { jobs, loading, error } = useFetchJobs(params, page);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
   return (
     <Container className="my-4">
+      <h1 className="mb-4">Welcome to the Job Portal</h1>
+      <Page page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading Jobs...</h1>}
       {error && <h1>Error, Please Try Refreshing...</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
       })}
+      <Page page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   );
 }
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -4,6 +4,7 @@ const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get-data",
   ERROR: "error",
+  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
 };
 const BASE_URL =
   "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
@@ -20,6 +21,8 @@ function reducer(state, action) {
         error: action.payload.error,
         jobs: [],
       };
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage };
     default:
       return state;
   }
@@ -41,8 +44,25 @@ export default function useFetchJobs(params, page) {
         if (axios.isCancel(e)) return;
         dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
       });
+    const cancelToken2 = axios.CancelToken.source();
+    axios
+      .get(BASE_URL, {
+        cancelToken: cancelToken2.token,
+        params: { markdown: true, page: page + 1, ...params },
+      })
+      .then((res) => {
+        dispatch({
+          type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+          payload: { hasNextPage: res.data.length !== 0 },
+        });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
     return () => {
       cancelToken1.cancel();
+      cancelToken2.cancel();
     };
   }, [params, page]);
   return state;
