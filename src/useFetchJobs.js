ad8358971693f753a5db7a634e25002224203aad
DhanushM12 <dhanushmukhi@gmail.com>
2020-10-16 19:56:08 +0530

    created a get request to api to fetch number of jobs

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,11 +1,12 @@
-import { useReducer } from "react";
-
+import { useReducer, useEffect } from "react";
+import axios from "axios";
 const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get-data",
   ERROR: "error",
-  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
 };
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
@@ -25,9 +26,20 @@ function reducer(state, action) {
 }
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
-  return {
-    jobs: [],
-    loading: false,
-    error: false,
-  };
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        //cancelToken: cancelToken1.token,
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+  }, [params, page]);
+  return state;
 }
